28513bd04c3919a50f54eb4095ace48be070b8e1
robin <nowrobin3@gmail.com>
2025-03-28 05:57:07 +0900

    feat :  비행기소개글 추가

--- src/app/components/planeItmes.tsx
+++ src/app/components/planeItmes.tsx
@@ -1,30 +1,85 @@
 'use client'
-
+import Image from "next/image"
+import { useState } from "react"
 type PlaneDetails = {
   images: string[],
   descriptions: string[],
+  odd: boolean
+  comingSoon?: boolean
 }
 
 
-export default function PlaneItems({ images, descriptions: desc }: PlaneDetails) {
-
+export default function PlaneItems({ images, descriptions: desc, odd, comingSoon }: PlaneDetails) {
+  const [imageIndex, setImageIndex] = useState(0)
+  const handlePrev = () => {
+    if (imageIndex === 0) {
+      setImageIndex(images.length - 1)
+    } else
+      setImageIndex(imageIndex - 1)
+  }
+  const handleNext = () => {
+    if (imageIndex === images.length - 1) {
+      setImageIndex(0)
+    } else
+      setImageIndex(imageIndex + 1)
+  }
   return (
-    <div className="flex flex-row">
-      <section>
-        {images[0]}
-      </section>
-      <article>
-        {/* <title>N49202</title>
-        <div>1978 Cessna 152</div>
-        <div>Lycoming O-235-L2C (115HP)</div>
-        <div>Flight Rule: IFR</div>
-        <div>Price:$120 /hr (Wet)</div> */}
-        <title>{desc[0]}</title>
-        <div>{desc[1]}</div>
-        <div>{desc[2]}</div>
-        <div>{desc[3]}</div>
-        <div>{desc[4]}</div>
-      </article >
+    <div className="flex flex-row rounded-3xl gap-20 w-full justify-center h-[20rem] items-center">
+      {
+        comingSoon ?
+          <div className="flex flex-row w-[1048px] justify-between h-full ">
+            <section className="relative flex flex-row justify-center items-center w-[480px] h-[18rem] rounded-xl shadow-2xl ">
+              <div>
+                <Image className="rounded-xl bg-transparent" src={images[imageIndex]} alt={"PlaneImages"} fill style={{ objectFit: 'cover', position: "absolute" }} /></div>
+            </section>
+            <article className="flex flex-col w-[480px] gap-2 h-full justify-center items-center">
+              <div className="text-5xl font-bold font-winky">Coming Soon ...</div>
+            </article >
+          </div>
+          :
+          odd ? <div className="flex flex-row w-[1048px] justify-between ">
+            <section className="relative flex flex-row justify-center items-center w-[480px] h-[18rem] rounded-xl shadow-2xl ">
+              <button className="absolute left-0 z-10 h-full rounded-l-xl" onClick={handlePrev}>
+                <Image src={'/icon/chev_left_white.svg'} alt={"chevIcon"} width={48} height={48} />
+              </button>
+              <div>
+                <Image className="rounded-xl bg-transparent" src={images[imageIndex]} alt={"PlaneImages"} fill style={{ objectFit: 'cover', position: "absolute" }} /></div>
+              <button className="absolute right-0  z-10 h-full rounded-r-xl" onClick={handleNext}>
+                <Image src={'/icon/chev_right_white.svg'} alt={"chevIcon"} width={48} height={48} />
+              </button>
+            </section>
+            <article className="flex flex-col w-[480px] gap-2 h-full justify-center">
+              <div className="text-5xl font-bold font-winky">N49202</div>
+              <div className="text-xl font-semibold text-gray-500">1977 Cessna 152 II</div>
+              <div className="text-xl font-semibold text-gray-500">Lycoming O-235-L2C (115HP)</div>
+              <div className="text-xl font-semibold text-gray-500">Flight Rule: IFR</div>
+              <div className="text-xl font-semibold text-gray-500">Price:$120 /hr (Wet)</div>
+              <div className="text-xl font-semibold text-gray-500">The Cessna 152 is a reliable, fuel-efficient two-seat trainer, perfect for safe and affordable flight training.</div>
+            </article >
+
+          </div> :
+            <div className="flex flex-row w-[1048px] justify-between ">
+              <article className="flex flex-col w-[480px] gap-2 h-full justify-center">
+                <div className="text-5xl font-bold font-winky">N4900L</div>
+                <div className="text-xl font-semibold text-gray-500">1980 Cessna 152 II</div>
+                <div className="text-xl font-semibold text-gray-500">Lycoming O-235-L2C (115HP)</div>
+                <div className="text-xl font-semibold text-gray-500">Flight Rule: IFR</div>
+                <div className="text-xl font-semibold text-gray-500">Price:$120 /hr (Wet)</div>
+                <div className="text-xl font-semibold text-gray-500">The Cessna 152 is a reliable, fuel-efficient two-seat trainer, perfect for safe and affordable flight training.</div>
+              </article >
+              <section className="relative flex flex-row justify-center items-center w-[480px] h-[18rem] rounded-xl shadow-2xl ">
+                <button className="absolute left-0 z-10 h-full rounded-l-xl" onClick={handlePrev}>
+                  <Image src={'/icon/chev_left_white.svg'} alt={"chevIcon"} width={48} height={48} />
+                </button>
+                <div>
+                  <Image className="rounded-xl bg-transparent" src={images[imageIndex]} alt={"PlaneImages"} fill style={{ objectFit: 'cover', position: "absolute" }} /></div>
+                <button className="absolute right-0  z-10 h-full rounded-r-xl" onClick={handleNext}>
+                  <Image src={'/icon/chev_right_white.svg'} alt={"chevIcon"} width={48} height={48} />
+                </button>
+              </section>
+            </div>
+      }
+
     </div >
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 'use client'
 import { useState } from "react";
 import Image from "next/image";
+import PlaneItems from "@/app/components/planeItmes";
 // import PlaneItems from "@/app/components/planeItmes";
 
 export default function Home() {
   const [btnOpen, setBtnOpen] = useState(false)
   return (
-    <div className="relative flex flex-col gap-2 w-full pt-6">
-      <section className=" flex flex-row justify-between items-center w-full px-3">
+    <div className="relative flex flex-col w-full">
+      <section className=" flex flex-row justify-between items-center w-full px-3 p-2">
         <div>
           <Image src={"/fullLogo.png"} alt={""} width={240} height={40} />
         </div>
-        <div className="flex-row hidden desktop:flex gap-5 mr-10">
-          <div>About</div>
-          <div>Contact</div>
-          <div>Schedule</div>
+        <div className="flex-row hidden tablet:flex desktop:flex gap-10 mr-10 justify-center items-center text-gray-600 font-semibold">
+          <div>Home</div>
+          <div>Become A Pilot</div>
+          <div>Aircraft</div>
+          <div>Instructor</div>
+          <div>About Us</div>
+          <button className="py-2 bg-black text-white px-3 rounded-xl">Contact Us</button>
         </div>
         <div className="mobile:hidden" onClick={() => setBtnOpen((prev) => (!prev))} >
           ==
@@ -41,11 +45,30 @@ export default function Home() {
         </div>
 
       </div>
-      {/* <main>
-        <section>
-          <PlaneItems images={[]} descriptions={""} name={""}></PlaneItems>
+      <main>
+        <section className=" flex flex-col gap-30 justify-center py-32">
+          <PlaneItems images={[
+            '/N49202/N49202_1.jpeg',
+            '/N49202/N49202_2.jpeg',
+            '/N49202/N49202_3.jpeg',
+            '/N49202/N49202_4.jpeg',
+            '/N49202/N49202_5.jpeg',
+            '/N49202/N49202_6.jpeg',
+          ]} descriptions={[]} odd={true} />
+          <PlaneItems images={[
+            '/N4900L/N4900L_1.jpeg',
+            '/N4900L/N4900L_2.jpeg',
+            '/N4900L/N4900L_3.jpeg',
+            '/N4900L/N4900L_4.jpeg',
+            '/N4900L/N4900L_5.jpeg',
+            '/N4900L/N4900L_6.jpeg'
+          ]} descriptions={[]} odd={false} />
+          <PlaneItems images={[
+            '/coming_soon.png'
+          ]} descriptions={[]} odd={true}
+            comingSoon={true} />
         </section>
-      </main> */}
+      </main>
     </div>
   );
 }
