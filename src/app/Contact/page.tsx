bc95cde3667a3d9a0f5968567eb42c472ba041da
robin <nowrobin3@gmail.com>
2025-03-31 15:53:54 +0900

    feat: Add Navigation

---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+export default function ContactPage() {
+  const router = useRouter()
+
+  return (
+    <div className="relative flex flex-row w-full">
+      <section className="fixed top-0 z-30 bg-white flex flex-row justify-between items-center w-full px-3 p-2" >
+        <div onClick={() => router.push('/')} >
+          <Image src={"/fullLogo.png"} alt={""} width={240} height={40} />
+        </div>
+        <div className="flex-row  hidden tablet:flex desktop:flex gap-10 mr-10 justify-center items-center text-gray-600 font-semibold cursor-default ">
+          <div className="hover:text-[#FFBD59]" onClick={() => router.push('/')} >Home</div>
+          <div className="hover:text-[#FFBD59]" onClick={() => router.push('/')}>Aircrafts</div>
+          <div className="hover:text-[#FFBD59]" onClick={() => router.push('/')}>Our Crew</div>
+          {/* <div>Become A Pilot</div> */}
+          {/* <div>About Us</div> */}
+          <button className="py-2 bg-[#FFBD59] text-black px-3 rounded-xl">Contact Us</button>
+        </div>
+      </section >
+      <section className="flex w-1/2">
+        <div className="relative w-full h-screen">
+          <Image src={"/mockDesign.png"} alt={""} fill style={{ objectFit: 'cover' }}></Image>
+        </div>
+      </section>
+      <section className="flex flex-col w-1/2  self-center">
+        <div className="flex flex-col p-20 gap-4">
+          <div className=" flex flex-row text-2xl">
+            Have questions about flight training, scheduling, or our programs? We’re here to help! Whether you’re ready to start your journey toward becoming a pilot or just need more information, feel free to reach out.
+          </div>
+          <div className="flex flex-row  gap-4 pl-8">
+            <Image src={'/icon/icon_Map.svg'} alt="" width={24} height={24} />
+            Location: 1395 Fairplex Dr. Hangar A-6,  La Verne, CA 91750
+          </div>
+          <div className="flex flex-row  gap-4 pl-8">
+            <Image src={'/icon/icon_Phone.svg'} alt="" width={24} height={24} /> Phone: (562) 266-6868
+          </div>
+          <div className="flex flex-row gap-4 pl-8">
+            <Image src={'/icon/icon_Mail.svg'} alt="" width={24} height={24} /> Email: info@skylogixaviation.com</div>
+          <div className="flex flex-row gap-4 pl-8">
+            <Image src={'/icon/icon_Clock.svg'} alt="" width={24} height={24} /> Hours: Mon-Fri 09:00 am - 04:00 pm
+          </div>
+        </div>
+      </section>
+    </div >
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,58 +2,77 @@
 import { useRef, useState } from "react";
 import Image from "next/image";
 import PlaneItems from "@/app/components/planeItmes";
+import { usePathname, useRouter } from "next/navigation";
 // import PlaneItems from "@/app/components/planeItmes";
 
 export default function Home() {
-  const [btnOpen, setBtnOpen] = useState(false)
+  // const [btnOpen, setBtnOpen] = useState(false)
   const [crew, setCrew] = useState(0)
 
 
-  const handleClick = () => {
+  const handleTopClick = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
-  const sectionRef = useRef<HTMLDivElement | null>(null);
+
+  const sectionAirCraftRef = useRef<HTMLDivElement | null>(null);
+  const sectionCrewRef = useRef<HTMLDivElement | null>(null);
+
   const scrollToSection = () => {
-    if (sectionRef.current) {
-      const y = sectionRef.current?.getBoundingClientRect().top + window.scrollY;
+    if (sectionAirCraftRef.current) {
+      const y = sectionAirCraftRef.current?.getBoundingClientRect().top + window.scrollY;
       const offset = 80; // 고정된 헤더 높이만큼
       window.scrollTo({ top: y - offset, behavior: 'smooth' });
     }
+    if (sectionCrewRef.current) {
+      const y = sectionCrewRef.current?.getBoundingClientRect().top + window.scrollY;
+      const offset = 40; // 고정된 헤더 높이만큼
+      window.scrollTo({ top: y - offset, behavior: 'smooth' });
+    }
+
   };
+
+
+
+  const handleNextCrew = () => {
+    if (crew == 2) {
+      setCrew(0)
+    } else {
+      setCrew(crew + 1)
+    }
+  }
+  const handlePrevCrew = () => {
+    if (crew == 0) {
+      setCrew(2)
+    } else {
+      setCrew(crew - 1)
+    }
+  }
+  const router = useRouter()
+  const pathname = usePathname()
+  console.log(pathname)
+
 
   return (
     <div className="relative flex flex-col w-full">
       <section className="fixed z-30 bg-white flex flex-row justify-between items-center w-full px-3 p-2">
-        <div>
+        <div onClick={() => router.push('/')}  >
           <Image src={"/fullLogo.png"} alt={""} width={240} height={40} />
         </div>
         <div className="flex-row  hidden tablet:flex desktop:flex gap-10 mr-10 justify-center items-center text-gray-600 font-semibold cursor-default ">
-          <div className="hover:text-[#FFBD59]" onClick={handleClick}>Home</div>
+          <div className="hover:text-[#FFBD59]" onClick={handleTopClick}>Home</div>
           <div className="hover:text-[#FFBD59]" onClick={scrollToSection}>Aircrafts</div>
-          <div>Our Crew</div>
-          <div>Become A Pilot</div>
-          <div>About Us</div>
-          <button className="py-2 bg-[#FFBD59] text-black px-3 rounded-xl">Contact Us</button>
+          <div className="hover:text-[#FFBD59]" onClick={scrollToSection}>Our Crew</div>
+          {/* <button disabled>Become A Pilot</button> */}
+          {/* <div>About Us</div> */}
+          <button className="py-2 bg-[#FFBD59] text-black px-3 rounded-xl" onClick={() => router.push('/Contact')} >Contact Us</button>
         </div>
-        <div className="mobile:hidden" onClick={() => setBtnOpen((prev) => (!prev))} >
-          ==
-        </div>
-        {
-          btnOpen &&
-          <div className="absolute flex flex-col gap-10 bg-blue-400 w-11/12 right-0 top-0">
-            <div onClick={() => setBtnOpen((prev) => (!prev))} >X</div>
-            <div>About</div>
-            <div>Contact</div>
-            <div>Schedule</div>
-          </div>
-        }
       </section>
       <div className="relative w-full h-[700px] flex">
         <Image src={"/landing_Image.png"} alt={""} style={{ objectFit: "fill" }} fill loading="lazy" />
         <div className="absolute flex flex-col justify-center items-center size-full gap-3">
           <div className="text-6xl font-bold">Learn to Fly</div>
           <div className="text-4xl ">in a way that is quick, easy, and efficient</div>
-          <button className="py-2 bg-[#FFBD59] text-black font-semibold px-3 rounded-xl mt-5">
+          <button className="py-2 bg-[#FFBD59] text-black font-semibold px-3 rounded-xl mt-5" onClick={() => router.push('/Contact')}>
             Contact Us
           </button>
         </div>
@@ -66,7 +85,7 @@ export default function Home() {
         <div className="flex-1 text-xl  font-medium justify-center  z-10 bg-white/40 p-4">where your aviation dreams take flight! We are dedicated to providing high-quality, affordable flight training in our well-maintained Cessna 152 aircrafts. Our mission is to make flying accessible while maintaining the highest safety standards, ensuring that every student can pursue their passion for aviation without financial barriers. Beyond training, we are committed to fostering a supportive and inspiring community where students and instructors work together to achieve their ultimate goal becoming airline transport pilots. Join us and start your journey to the skies today!
         </div>
       </div>
-      <section ref={sectionRef} className="bg-gray-200/40">
+      <section ref={sectionAirCraftRef} className="bg-gray-200/40">
         <div className={"pl-60 text-6xl font-bold text-[#FFBD59] my-12 "}>Aircrafts</div>
         <div className=" flex flex-col gap-30 justify-center pb-32">
           <PlaneItems images={[
@@ -91,7 +110,7 @@ export default function Home() {
             comingSoon={true} />
         </div>
       </section>
-      <section className="flex flex-col items-center  bg-white w-full h-[700px]">
+      <section ref={sectionCrewRef} className="flex flex-col items-center  bg-white w-full h-[700px]">
         <div className={"text-6xl font-bold text-[#FFBD59] my-12 "}>Our Crew</div>
         <div className="flex flex-row gap-5 mb-10">
           <button onClick={() => setCrew(0)} className={`transition-all duration-400 ${crew === 0 ? "text-xl font-bold text-[#FFBD59] p-2 " : "text-gray-400 "}`}>Founder</button> /
@@ -102,20 +121,20 @@ export default function Home() {
           {
             crew === 0 ?
               <div className="flex flew-row gap-3">
-                <button onClick={() => setCrew(crew - 1)}>
+                <button onClick={handlePrevCrew}>
                   <Image src={"/icon/chev_left.svg"} alt={""} width={40} height={40} />
                 </button>
                 <div className="relative w-44 h-56 border">
                   <Image src={"/coming_soon.png"} alt={""} fill
                     style={{ objectFit: 'cover' }} />
                 </div>
-                <button onClick={() => setCrew(crew + 1)}>
+                <button onClick={handleNextCrew}>
                   <Image src={"/icon/chev_right.svg"} alt={""} width={40} height={40} />
                 </button>
               </div> :
               crew === 1 ?
                 <div className="flex flew-row gap-3">
-                  <button onClick={() => setCrew(crew - 1)}>
+                  <button onClick={handlePrevCrew}>
                     <Image src={"/icon/chev_left.svg"} alt={""} width={40} height={40} />
                   </button>
                   <div className="relative w-44 h-56 border">
@@ -130,12 +149,12 @@ export default function Home() {
                     <Image src={"/coming_soon.png"} alt={""} fill
                       style={{ objectFit: 'cover' }} />
                   </div>
-                  <button onClick={() => setCrew(crew + 1)}>
+                  <button onClick={handleNextCrew}>
                     <Image src={"/icon/chev_right.svg"} alt={""} width={40} height={40} />
                   </button>
                 </div> :
                 <div className="flex flew-row gap-3">
-                  <button onClick={() => setCrew(crew - 1)}>
+                  <button onClick={handlePrevCrew}>
                     <Image src={"/icon/chev_left.svg"} alt={""} width={40} height={40} />
                   </button>
                   <div className="relative w-44 h-56 border">
@@ -150,8 +169,7 @@ export default function Home() {
                     <Image src={"/coming_soon.png"} alt={""} fill
                       style={{ objectFit: 'cover' }} />
                   </div>
-
-                  <button onClick={() => setCrew(crew + 1)}>
+                  <button onClick={handleNextCrew}>
                     <Image src={"/icon/chev_right.svg"} alt={""} width={40} height={40} />
                   </button>
                 </div>
